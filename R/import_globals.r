21fe8cebb1b06bf946e03da50ea1317ef6dc17ee
kasperwelbers <kasperwelbers@gmail.com>
2017-04-26 18:41:35 +0200

    fixed dependencies

--- R/import_globals.r
+++ R/import_globals.r
@@ -1,7 +1,6 @@
 ## This is a dummy function to let roxygen2 create the NAMESPACE but include additional stuff
 
 #' @import Matrix
-#' @import igraph
 #' @import data.table
 #' @importFrom Rcpp evalCpp
 #' @useDynLib corpustools
